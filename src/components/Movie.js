e7f2c8561fd4cd2984c197367731ffc5b8a59484
JennieBytes <kimjh0995@gmail.com>
2024-11-05 21:03:55 -0500

    finishing this course

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
 				{/* 'Link to=' is used instead of 'a href=' */}
 				<Link to={`/movie/${id}`}>{title}</Link>
 			</h2>
-			<p>{summary}</p>
+			<p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
 			<ul>
 				genre:
 				{genres?.map((genre) => (
